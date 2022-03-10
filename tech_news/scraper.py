b48d05315207fa1afce1e755349c413701bfbba4
Samuel Constantino <samuelicapuidados@gmail.com>
2022-03-10 17:50:58 -0300

    implementa requisito 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -34,7 +34,12 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    next_page_link = selector.css("a.tec--btn::attr(href)").get()
+
+    if next_page_link is not None:
+        return next_page_link
 
 
 # Requisito 4
