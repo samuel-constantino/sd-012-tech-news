177aac1efd86d851b0531759f131fc2765861452
Samuel Constantino <samuelicapuidados@gmail.com>
2022-03-11 10:45:57 -0300

    implementa requisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -44,7 +44,51 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("head link[rel=canonical]::attr(href)").get()
+
+    title = selector.css("h1.tec--article__header__title::text").get()
+
+    timestamp = selector.css("time#js-article-date::attr(datetime)").get()
+
+    writer = selector.css(".z--font-bold *::text").get()
+    if writer:
+        writer = writer.strip()
+
+    shares_count = selector.css("div.tec--toolbar__item::text").get()
+    if shares_count:
+        shares_count = int(shares_count[:-15].strip())
+    else:
+        shares_count = 0
+
+    comments_count = selector.css("button#js-comments-btn::text").getall()[1]
+    comments_count = int(comments_count[:-13].strip())
+
+    summary = "".join(selector.css(
+        ".tec--article__body > p:nth-of-type(1) *::text"
+    ).getall())
+
+    sources_list = selector.css("div.z--mb-16 div a::text").getall()
+    sources = []
+    for source in sources_list:
+        sources.append(source.strip())
+
+    category_list = selector.css("div#js-categories a::text").getall()
+    categories = []
+    for category in category_list:
+        categories.append(category.strip())
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
 
 
 # Requisito 5
