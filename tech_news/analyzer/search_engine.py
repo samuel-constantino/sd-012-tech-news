8b01c887f4ee012c2d2de93e861d4d2e88a02c22
Samuel Constantino <samuelicapuidados@gmail.com>
2022-03-11 16:00:51 -0300

    implementa requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,40 @@
+import datetime
+import re
 from ..database import search_news
+
+
+def format_tech_news_list(tech_news_list):
+    formated_list = []
+    for tech_new in tech_news_list:
+        formated_list.append((tech_new["title"], tech_new["url"]))
+    return formated_list
 
 
 # Requisito 6
 def search_by_title(title):
     tech_news_list = search_news({"title": {"$regex": title, "$options": "i"}})
-    result = []
-    for tech_new in tech_news_list:
-        result.append((tech_new["title"], tech_new["url"]))
-    return result
+    return format_tech_news_list(tech_news_list)
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    splitted_date = date.split("-")
+    try:
+        datetime.datetime(
+            year=int(splitted_date[0]),
+            month=int(splitted_date[1]),
+            day=int(splitted_date[2])
+        )
+
+        # Por que essa solução não funciona?
+        # if re.search(r'[\d]{4}-[\d]{2}-[\d]{2}', date) is None:
+        #     raise ValueError("Data inválida")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    date_regex = re.compile(f"^{date}")
+    tech_news_list = search_news({"timestamp": date_regex})
+    return format_tech_news_list(tech_news_list)
 
 
 # Requisito 8
